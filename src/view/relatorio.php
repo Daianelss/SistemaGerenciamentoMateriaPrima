d8f06c2ee114d238c584692ecaaf0024f5d667f7
Natã Cooper <87046228+NatanCooper@users.noreply.github.com>
2023-04-23 18:44:15 -0300

    Feature/nata (#4)
    * adição dos botões funcionais voltar e imprimir
    
    * personalização das informaçoes na pagina relatorio
    
    * personalização da pagina relatorio
    
    ---------
    
    Co-authored-by: Daianelss <daianelucena@hotmail.com>

--- src/view/relatorio.php
+++ src/view/relatorio.php
@@ -21,8 +21,6 @@ $relatorios = [
   ['28/01/2023', '21.52', '2 pulseiras', '22.01', '-0.49'],
   ['28/09/2023', '21.52', 'Pulseira azul', '22.01', '-0.49'],
   ['28/01/2015', '21.52', 'Pulseira', '22.01', '-0.49'],
-  
-
 ];
 
 ?>
@@ -38,32 +36,44 @@ $relatorios = [
 </head>
 
 <body>
-  <div>
-  <h1 class ="container, row, col text-center">Sistema de Gerenciamento de Materiais</h>
-</div>
-    <form>
-      <select class="form-row, border-primary border-3"  name="todos">
-        <?php
-        $funcionarios = array("Todos", "",);
-        foreach ($funcionarios as $funcionario) {
-          echo "<option value='$funcionario'>$funcionario</option>";
-        }
-        ?>
-      </select>
+  <h1 class="text-center m-5 mx-auto">Sistema de Gerenciamento de Materiais</h1>
+  <div class="justify-content-center">
 
-      <select class="form-row, border-primary border-3" name="movimentos">
-        <?php
-        $movimentos = array("peróxido", "politriz", "magneto", "ródio");
-        foreach ($movimentos as $movimento) {
-          echo "<option value='$movimento'>$movimento</option>";
-        }
-        ?>
 
-      </select>
 
-      <input class= "form-row" type="date" min="2022-01-01" max="2030-01-01">
+    <form class="form-row m-5">
+      <div class="mb-2">
+        <label for="funcionarios">Funcionários:</label>
+        <select class="form-row rounded" id="funcionarios">
+          <option value="todos">Todos</option>
+          <option value="joao">João</option>
+          <option value="lucas">Lucas</option>
+          <option value="matheus">Matheus</option>
+        </select>
+      </div>
 
-      <!--
+      <div class="mb-2">
+        <label for="materiais">Materiais:</label>
+        <select class="form-row rounded" id="materiais">
+          <option value="peroxido">Peroxido</option>
+          <option value="politriz">Politriz</option>
+          <option value="magneto">Magneto</option>
+          <option value="rodio">Ródio</option>
+          <option value="rodio">Ródio negro</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="periodo">Periodo: </label>
+        <input class="form-row rounded mb-2" type="date" min="2022-01-01" max="2030-01-01">
+      </div>
+
+      <button class="btn btn-secondary " type="submit">Pesquisar</button>
+
+    </form>
+  </div>
+
+  <!--
       <div id="date-picker-example" class="md-form md-outline input-with-post-icon datepicker" inline="true">
         <input placeholder="Select date" type="text" id="example" class="form-row">
         <i class="fas fa-calendar input-prefix"></i>
@@ -76,8 +86,8 @@ $relatorios = [
     -->
 
 
-      <button class="btn btn-info" type="submit">Pesquisar</button>
-    </form>
+
+  <div class="m-5">
     <table class="table">
       <thead>
         <tr>
@@ -89,7 +99,7 @@ $relatorios = [
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($relatorios as $relatorio): ?>
+        <?php foreach ($relatorios as $relatorio) : ?>
 
 
           <tr>
@@ -115,27 +125,55 @@ $relatorios = [
 
     </table>
 
-    <div >      
-    <button class="btn btn-info" onclick="window.location.href='http://localhost/src/pages/home/index.php'">Voltar</button>
-    </div>
-    
-
-    <div id="d">  
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <button class="btn btn-secondary" onclick="window.location.href='http://localhost/src/pages/home/index.php'">Voltar</button>
+        </div>
+        <div class="col">
           <script language="javascript">
-          function imprime (text) {
-              text=document
+            function imprime(text) {
+              text = document
               print(text)
-          } 
+            }
           </script>
 
-      <form>
-        <input class="btn btn-info" type="button" value="Imprimir" name="Imprimir" onclick="imprime()"/>  
+          <form>
+            <input class="btn btn-secondary" type="button" value="Imprimir" name="Imprimir" onclick="imprime()" />
+          </form>
+        </div>
+      </div>
+
+    </div>
+
+
+    <!--      
+    <div class="btn btn-secondary">
+      <div>
+        <button onclick="window.location.href='http://localhost/src/pages/home/index.php'">Voltar</button>
+      </div>
+
+
+      <div id="d">
+        <script language="javascript">
+          function imprime(text) {
+            text = document
+            print(text)
+          }
+        </script>
+
+        <form>
+          <input type="button" value="Imprimir" name="Imprimir" onclick="imprime()" />
         </form>
-        </div>    
-    <?php include '../view/bootstrap_foot.php'; ?>
+      </div>
+
+    </div>
+        -->
+  </div>
+  <?php include '../view/bootstrap_foot.php'; ?>
+
 
 </body>
 
-
-</html> 
+</html>
 
