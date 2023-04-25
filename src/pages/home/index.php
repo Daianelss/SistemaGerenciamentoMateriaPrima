3315b8a3e399f014e11acd7726a1cb027d65ec8a
Natã Cooper <natancooper@yahoo.com.br>
2023-04-24 23:02:21 -0300

    personalização das paginas index.php, cadastro-funcionario.php e relatorio

--- src/pages/home/index.php
+++ src/pages/home/index.php
@@ -1,36 +1,50 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Página Inicial</title>
-<?php
-require_once "../../config/db.php";
-include '../../view/bootstrap_head.php';
-
-?>
-</head>
-<body>
-    <h1>Bem-vindo ao Sistema de Movimentos</h1>
-    <h2>Escolha uma opção abaixo:</h2>
-    <!-- Botões para as opções -->
-    <a href="../../view/relatorio.php" class="btn btn-info">Relatório</a>
-    <a href="../../view/banca.php" class="btn btn-info">Bancas</a>
-    <a href="../../view/cadastro-funcionario.php" class="btn btn-info">Funcionários</a>
-    <a href="../../view/cadastro-materia-prima.php" class="btn btn-info">Matéria Prima</a>
-    <a href="peroxido.html" class="btn btn-info">Peroxido</a>
-    <a href="politriz.html" class="btn btn-info">Politriz</a>
-    <a href="conserto.html" class="btn btn-info">Conserto</a>
-    <a href="rodio_branco.html" class="btn btn-info">Ródio Branco</a>
-    <a href="rodio_negro.html" class="btn btn-info">Ródio Negro</a>
-    <a href="magneto.html" class="btn btn-info">Magneto</a>
-    <a href="peso_medio.html" class="btn btn-info">Peso Médio</a>
-
-<!-- Botão para teste -->
-    <a href="../../teste/teste.html" class="btn btn-danger">Testes</a>
-
     <?php
-include '../../view/bootstrap_foot.php';
+    require_once "../../config/db.php";
+    include '../../view/bootstrap_head.php';
 
-?>
+    ?>
+</head>
+<header class="bg-secondary p-3">
+    <h1 class="text-center mb-5 text-white">Bem-vindo ao Sistema de Movimentos</h1>
+</header>
 
+<body>
+    <div class="mx-auto mt-5">
+
+        <h2 class="text-center" style="height: 180px;">Escolha uma opção abaixo:</h2>
+        <!-- Botões para as opções -->
+        <div class="col-md-12 d-flex align-items-center justify-content-center">
+            <a href="../../view/relatorio.php" class="btn btn-secondary" style="margin-right: 10px;">Relatório</a>
+            <a href="../../view/banca.php" class="btn btn-secondary" style="margin-right: 10px;">Bancas</a>
+            <a href="../../view/cadastro-funcionario.php" class="btn btn-secondary" style="margin-right: 10px;">Funcionários</a>
+            <a href="../../view/cadastro-materia-prima.php" class="btn btn-secondary" style="margin-right: 10px;">Matéria Prima</a>
+            <a href="peroxido.html" class="btn btn-secondary" style="margin-right: 10px;">Peroxido</a>
+            <a href="politriz.html" class="btn btn-secondary" style="margin-right: 10px;">Politriz</a>
+            <a href="conserto.html" class="btn btn-secondary" style="margin-right: 10px;">Conserto</a>
+            <a href="rodio_branco.html" class="btn btn-secondary" style="margin-right: 10px;">Ródio Branco</a>
+            <a href="rodio_negro.html" class="btn btn-secondary" style="margin-right: 10px;">Ródio Negro</a>
+            <a href="magneto.html" class="btn btn-secondary" style="margin-right: 10px;">Magneto</a>
+            <a href="peso_medio.html" class="btn btn-secondary" style="margin-right: 10px;">Peso Médio</a>
+        </div>
+    </div>
+
+
+
+    <!-- Botão para teste 
+    <a href="../../teste/teste.html" class="btn btn-danger">Testes</a>
+    -->
+    <?php
+    include '../../view/bootstrap_foot.php';
+
+    ?>
+    <footer class="bg-secondary p-3 container-fluid fixed-bottom ">
+        <h6 class="text-white mt-5 text-center  ">Todos os Direitos Reservados © 2023</h6>
+    </footer>
 </body>
+
 </html>
--- src/view/cadastro-funcionario.php
+++ src/view/cadastro-funcionario.php
@@ -12,24 +12,30 @@ $view = new CadastroFuncionarioView();
     <?php include '../view/bootstrap_head.php'; ?>
 </head>
 
+<header class="bg-secondary p-3">
+    <h1 class="text-center mb-5 text-white">Cadastro de Funcionários</h1>
+</header>
+
 <body>
 
-    <h1>Cadastro de Funcionário</h1>
+
     <form method="post" name="formSalvarEditar" action="cadastro-funcionario.php">
-        <label for="funcionario">Funcionário:</label>
+        <label class= "mt-5"for="funcionario">Funcionário:</label>
         <input type="text" id="nomeFuncionario" name="nomeFuncionario" required><br>
         <input type="hidden" id="idFuncionario" name="idFuncionario"><br>
-        <input type="submit" value="Salvar" name="salvar">
+        <input class= "btn btn-secondary" type="submit" value="Salvar" name="salvar">
     </form>
 
     <?= $view->dispararAcao() ?>
 
     <form method="post" name="formTabela" action="cadastro-funcionario.php">
 
-        <table class='table table-striped'>
+        <table class='table table-secondary table-striped table-hover mt-5'>
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
+                <th>Edição</th>
+                <th>Status</th>
             </tr>
             <?= $view->renderizarTabela() ?>
         </table>
@@ -48,6 +54,7 @@ $view = new CadastroFuncionarioView();
     <?php
     include '../view/bootstrap_foot.php';
     ?>
+
 </body>
 
 </html>
